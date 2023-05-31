7f3bbff60f01e09a5e35ddfa16356143dc4f40f9
Abin vinoy <134992947+abinzd@users.noreply.github.com>
2023-05-31 20:12:05 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -8,13 +8,9 @@ with open('app.json') as json_file:
 
 
 class cred:
-    #BOT_NAME = os.getenv("BOT_NAME")
-    #BOT_TOKEN = os.getenv("BOT_TOKEN") 
-    #API_ID = os.getenv( "API_ID") 
-    #API_HASH = os.getenv("API_HASH")  
-    #DB_URL = os.getenv("DB_URL")  
-    DB_URL=data['env']['DB_URL']['value']
-    BOT_TOKEN=data['env']['BOT_TOKEN']['value']
-    API_ID=data['env']['API_ID']['value']
-    API_HASH=data['env']['API_HASH']['value']
-    BOT_NAME=data['env']['BOT_NAME']['value']
+    BOT_NAME = os.getenv("BOT_NAME")
+    BOT_TOKEN = os.getenv("BOT_TOKEN") 
+    API_ID = os.getenv( "API_ID") 
+    API_HASH = os.getenv("API_HASH")  
+    DB_URL = os.getenv("DB_URL")  
+    
